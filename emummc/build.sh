00f6f4d67cb64d193e593dd72f1d5c7b66b67a9a
Nico Christmann <77099168+Happynico7504@users.noreply.github.com>
2026-06-27 17:50:10 +0200

    Update build script to use hactool commands

--- emummc/build.sh
+++ emummc/build.sh
@@ -2,8 +2,16 @@
 
 set -e
 
+git clone https://github.com
+cd hactool
+cp config.mk.template config.mk
+make
+sudo make install
+
+cd ..
+
 make clean
 make -j
-./hactool.exe -t kip emummc.kip --uncompressed emummc_unpacked.kip
-python2.7 tools/kip1converter.py emummc_unpacked.kip emummc.data
+hactool -t kip emummc.kip --uncompressed emummc_unpacked.kip
+python tools/kip1converter.py emummc_unpacked.kip emummc.data
 cat emummc.caps emummc.data > emummc.kipm
