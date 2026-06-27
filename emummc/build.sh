ec45f54ba6ed7d9fa8fdf4c1a24d2ca0ce9afb3e
Nico Christmann <77099168+Happynico7504@users.noreply.github.com>
2026-06-27 17:54:55 +0200

    Fix build script for hactool usage and installation
    Updated build script to use relative path for hactool and fixed installation command.

--- emummc/build.sh
+++ emummc/build.sh
@@ -6,12 +6,11 @@ git clone https://github.com/SciresM/hactool.git
 cd hactool
 cp config.mk.template config.mk
 make
-sudo make install
 
 cd ..
 
 make clean
 make -j
-hactool -t kip emummc.kip --uncompressed emummc_unpacked.kip
+./hactool/hactool -t kip emummc.kip --uncompressed emummc_unpacked.kip
 python tools/kip1converter.py emummc_unpacked.kip emummc.data
 cat emummc.caps emummc.data > emummc.kipm
